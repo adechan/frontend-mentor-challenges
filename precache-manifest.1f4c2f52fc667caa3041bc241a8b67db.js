b5910448d24b4a881beaf60dc5cf3cfb75746992
adechan <ade.enter@yahoo.com>
2020-09-27 18:44:52 +0300

    Updates

---
+++ precache-manifest.1f4c2f52fc667caa3041bc241a8b67db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d98778b2732a3af1e7236072d62824",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/index.html"
+  },
+  {
+    "revision": "878fcb0b0e85b0523634",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/css/main.13519ebe.chunk.css"
+  },
+  {
+    "revision": "c09f0271e703109f0000",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/js/2.439ba925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/js/2.439ba925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878fcb0b0e85b0523634",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/js/main.d2cf968b.chunk.js"
+  },
+  {
+    "revision": "5f70e73795a40dc2ed9a",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/js/runtime-main.51fed54c.js"
+  },
+  {
+    "revision": "1ccf745a6ea3ba0e58ee2a962ece2fbd",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/media/bg-pattern-bottom-desktop.1ccf745a.svg"
+  },
+  {
+    "revision": "02b87d6740fcff00e2d794d4db5f4f8d",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/media/bg-pattern-bottom-mobile.02b87d67.svg"
+  },
+  {
+    "revision": "0e1f3813427daeaeebd6bc4f0c89e2a4",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/media/bg-pattern-top-desktop.0e1f3813.svg"
+  },
+  {
+    "revision": "f55aa9cca88e83b31f78ae4ff7033063",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/media/bg-pattern-top-mobile.f55aa9cc.svg"
+  },
+  {
+    "revision": "7f398b9ae487dcea305eeccf231bfd7b",
+    "url": "/Frontend-Mentor-Challenges/social-proof-section/static/media/icon-star.7f398b9a.svg"
+  }
+]);
