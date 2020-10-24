b52b1e10e95b8cffd4659f7cca7e1b8e31ec1181
adechan <ade.enter@yahoo.com>
2020-10-24 23:59:32 +0300

    Updates on desktop UI

--- ping-coming-soon-page/src/components/Input/Input.js
+++ ping-coming-soon-page/src/components/Input/Input.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Input.css";
 
 function Input() {
+  const handleEmail = (event) => {
+    event.preventDefault();
+  };
   return (
     <form className="input">
       <input type="text" placeholder="Your email address..." />
-      <button>Notify Me</button>
+      <button onClick={handleEmail}>Notify Me</button>
     </form>
   );
 }
