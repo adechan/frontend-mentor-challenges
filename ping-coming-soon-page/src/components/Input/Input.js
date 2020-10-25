2e13259332e2df4d4c58e305bbe6dcce0eb5470b
adechan <ade.enter@yahoo.com>
2020-10-25 16:15:08 +0200

    Added email verification

--- ping-coming-soon-page/src/components/Input/Input.js
+++ ping-coming-soon-page/src/components/Input/Input.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
 function Input() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   const handleEmail = (event) => {
     event.preventDefault();
+
+    setEmail(email);
+    if (email === "" || validateEmail(email) === false) {
+      setError("Please provide a valid email address");
+      console.log(error);
+    } else {
+      setEmail("");
+      setError("");
+    }
   };
+
   return (
-    <form className="input">
-      <input type="text" placeholder="Your email address..." />
-      <button onClick={handleEmail}>Notify Me</button>
-    </form>
+    <>
+      <form className={"input " + (error ? "error" : null)}>
+        <div className="input__outer">
+          <input
+            type="text"
+            placeholder="Your email address..."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <p className={error ? "error_message" : null}>{error}</p>
+        </div>
+
+        <button type="submit" onClick={handleEmail}>
+          Notify Me
+        </button>
+      </form>
+    </>
   );
 }
 
