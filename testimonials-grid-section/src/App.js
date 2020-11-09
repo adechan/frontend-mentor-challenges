a4a57cdc8029a7cb275d685b37a78c6395c5b161
adechan <ade.enter@yahoo.com>
2020-11-09 12:24:37 +0200

    Update App.js

--- testimonials-grid-section/src/App.js
+++ testimonials-grid-section/src/App.js
@@ -80,7 +80,7 @@ function App() {
         verified="Verified Graduate"
         description="Awesome teaching support from TAs who did the bootcamp themselves. 
         Getting guidance from them and learning from their experiences was easy."
-        quote="he staff seem genuinely concerned about my progress which I find really 
+        quote="The staff seem genuinely concerned about my progress which I find really 
         refreshing. The program gave me the confidence necessary to be able to go out
         in the world and present myself as a capable junior developer. The standard 
         is above the rest. You will get the personal attention you need from an incredible
